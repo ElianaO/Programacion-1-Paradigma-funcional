3ab22664d54ba9db3529cf4439b22d228ba1ed47
Eli <ElianaO@users.noreply.github.com>
2018-02-28 15:34:33 -0500

    Add files via upload
    Ejercicios básicos paradigma funcional usando Scheme

---
+++ clase 3.rkt
@@ -0,0 +1,12 @@
+(+ 4 5);-> 10   
+;(<Nombre funcion> <argumento 1>entero, <argumento2>entero)-> entero
+(+ 4 5 6 7)
+;(<Nombre funcion> <argumento1 >entero,......., <argumento4>entero)-> entero
+; (+ 4 5) + (+ 6 7) error #<procedure:+>
+(+ (+ 4 5) (+ 6 7))
+(abs -5) ; (<nombre fucion> <argumento>entero)->entero positivo
+(- 10 4)
+(- 4 10)
+(abs -10);->10
+(- 4 10) ; -> -6
+(abs(- 4 10));-> 6
